83e66fc3618a2011b35b74df0020b85000ccd682
Arquimago <thales@alvesnet.net>
2017-01-11 14:59:32 -0300

    ed2 fazendo

--- EDII/Ex1/exercicio1.c
+++ EDII/Ex1/exercicio1.c
@@ -4,11 +4,38 @@
 #include <math.h>
 #include <time.h>
 
-void lerS(char *arq, int chave){}
+int lerS(char *arq, int chave){
+	FILE *arquivo;
+	int temp,flag;
+	flag=1;
+	arquivo = fopen(arq, "r");
+	while(fscanf(arquivo,"%d",temp)!=EOF){
+		fscanf(arquivo,"%d", temp);
+		if(temp == chave) {
+			printf("%d", temp);
+			flag=0;
+			break;
+		}
+	}
+	if(flag){
+		printf("Chave não encontrada");
+		temp = -1;
+	}
+	fclose(arquivo);
+	return temp;
+}
 
-void gravarS(char *arq, int chave){}
+void gravarS(char *arq, int chave){
+	FILE *arquivo;
+	arquivo = fopen(arq, "a+");
+	fprintf(arquivo,"%d %d\n",chave, chave);
+	fclose(arquivo);
+}
 
-void modificarS(char *arq, int chave){}
+void modificarS(char *arq, int chave){
+	
+	
+}
 
 void excluirS(char *arq, int chave){}
 
@@ -31,9 +58,16 @@ void excluirD(char *arq, int chave){}
 
 int main(){
 	FILE *arquivo;
-	
-	arquivo = fopen("arquivo.txt", "w+");
-	
+	arquivo = fopen("seq.txt", "a+");
 	fclose(arquivo);
-	return 42;
+	arquivo = fopen("ind.txt", "a+");
+	fclose(arquivo);
+	arquivo = fopen("ind.ind", "a+");
+	fclose(arquivo);
+	arquivo = fopen("dir.txt", "a+");
+	fclose(arquivo);
+	
+	
+	
+	return 0;
 }
