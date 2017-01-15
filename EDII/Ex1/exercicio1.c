2b433b24c374534f57d4cb1142809d506069f5c5
ELTON MOREIRA CARVALHO <eltonmc@dcomp.ufs.br>
2017-01-15 14:10:55 -0300

    update edII

--- EDII/Ex1/exercicio1.c
+++ EDII/Ex1/exercicio1.c
@@ -4,40 +4,67 @@
 #include <math.h>
 #include <time.h>
 
-int lerS(char *arq, int chave){
-	FILE *arquivo;
-	int temp,flag;
-	flag=1;
-	arquivo = fopen(arq, "r");
-	while(fscanf(arquivo,"%d",temp)!=EOF){
-		fscanf(arquivo,"%d", temp);
-		if(temp == chave) {
-			printf("%d", temp);
+#define TAM 100
+
+char* createLine(char* chave, char* reg){
+   	int aux, i;
+    char *string = (char *) malloc(sizeof(char) * TAM);
+    strcpy(string,chave);
+    strcat(string," ");
+    strcat(string,reg);
+    aux = strlen(string);
+    for (i=aux;i<TAM;i++)
+        string[i] = ' ';
+    return string;
+}
+
+int lerS(char *arq, char *chave){
+	FILE *arquivo = fopen(arq, "r");
+	int temp,linha=0,flag=1;
+    char index[TAM],reg[TAM];
+    while(fscanf(arquivo,"%s",&index)!=EOF){
+		fscanf(arquivo,"%s", &reg);
+		if(strcmp(index,chave) == 0) {
+			printf("Registro encontrado: %s\n", reg);
 			flag=0;
 			break;
 		}
+		linha++;
 	}
 	if(flag){
 		printf("Chave não encontrada");
 		temp = -1;
 	}
 	fclose(arquivo);
-	return temp;
+	return linha;
 }
 
-void gravarS(char *arq, int chave){
+void gravarS(char *arq, char *chave){
 	FILE *arquivo;
 	arquivo = fopen(arq, "a+");
-	fprintf(arquivo,"%d %d\n",chave, chave);
+	char* string = createLine(chave, chave);
+	fprintf(arquivo,"%s\n", string);
 	fclose(arquivo);
 }
 
-void modificarS(char *arq, int chave){
-	
-	
+void modificarS(char *arq, char *chave, char *new_reg){
+	FILE *arquivo = fopen(arq, "r+");
+	int linha = lerS("seq.txt", chave);
+	char* string = createLine(chave,new_reg);
+	fseek(arquivo, linha*100, SEEK_SET);
+	fprintf(arquivo, "%s\n", string);
 }
 
-void excluirS(char *arq, int chave){}
+void excluirS(char *arq, char *chave){
+	FILE *arquivo = fopen(arq, "r+");
+	int linha = lerS("seq.txt", chave);
+	char* string;
+	char index[TAM],reg[TAM], aux[TAM];
+	fseek(arquivo, linha*100, SEEK_SET);
+	string = createLine("","");
+	fprintf(arquivo, "%s\n", string);
+    //Falta a função para ir deslocando as linhas
+}
 
 void lerI(char *arq, int chave){}
 
@@ -66,8 +93,6 @@ int main(){
 	fclose(arquivo);
 	arquivo = fopen("dir.txt", "a+");
 	fclose(arquivo);
-	
-	
-	
-	return 0;
+
+  return 0;
 }
