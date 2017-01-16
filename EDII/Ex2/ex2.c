b744ad57e15367f74e073db3cb8bee94527d22f5
Thales Francisco <thales@alvesnet.net>
2017-01-16 11:17:50 -0300

    esqueleto exercicio 2

---
+++ EDII/Ex2/ex2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+int lista1 = {26,76,39,61,25,10,16,70,53};
+int lista3 = {967,875,985,214,357,248,137,864,247,135};
+
+void hash1(){
+	
+	
+	return;
+}
+
+void hash2(){
+	
+	
+	return;
+}
+
+void hash3(){
+	
+	
+	return;
+}
+
+void hash4(){
+	
+	
+	return;
+}
+
+int main(){
+	
+	
+	
+	
+	return 0;
+}
+
