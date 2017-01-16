eb43818b088c57bf80c971534ec2a367bd7636ab
Thales Francisco <thales@alvesnet.net>
2017-01-16 11:39:23 -0300

    esqueleto melhorado

--- EDII/Ex2/ex2.c
+++ EDII/Ex2/ex2.c
@@ -4,36 +4,69 @@
 #include <math.h>
 #include <time.h>
 
-int lista1 = {26,76,39,61,25,10,16,70,53};
-int lista3 = {967,875,985,214,357,248,137,864,247,135};
+int lista1 = {26,76,39,61,25,10,16,70,53}; //9 elementos
+int lista3 = {967,875,985,214,357,248,137,864,247,135}; //10 elementos
 
-void hash1(){
-	
-	
-	return;
+int hash1(int num){
+	int hash = 0;
+	hash=num%7;
+	return hash;
 }
 
-void hash2(){
-	
-	
-	return;
+int desloc1(int num){
+	int hash = 0;
+	hash=(num%2)+1;
+	return hash;
 }
 
-void hash3(){
-	
-	
-	return;
+int hash2(int num){
+	int hash = 0;
+	hash=num%11;
+	return hash;
 }
 
-void hash4(){
-	
-	
-	return;
+int desloc2(int num){
+	int hash = 0;
+	hash=(num%2)+1;
+	return hash;
+}
+
+int hash3(int num){
+	int hash = 0;
+	hash=num%13;
+	return hash;
+}
+
+int desloc3(int num){
+	int hash = 0;
+	hash=(num%4)+1;
+	return hash;
+}
+
+int hash4(int num){
+	int hash = 0;
+	hash=num%17;
+	return hash;
+}
+
+int desloc4(int num){
+	int hash = 0;
+	hash=(num%3)+1;
+	return hash;
 }
 
 int main(){
+	printf("Lista de Exercícios - Parte 2\n");
+	printf("Exercício 1\n");
 	
 	
+	printf("Exercício 2\n");
+	
+	
+	printf("Exercício 3\n");
+	
+	
+	printf("Exercício 4\n");
 	
 	
 	return 0;
